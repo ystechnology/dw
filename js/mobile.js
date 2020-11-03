a1443f1a4f7d0b2e5303d8ddee20da66a7f3df8e
yoshiki <49330024+ystechnology@users.noreply.github.com>
2020-11-04 03:01:40 +0900

    init
    first

---
+++ js/mobile.js
@@ -0,0 +1,22 @@
+class MobileMenu{
+    constructor(){
+        this.DOM = {};
+        this.DOM.btn = document.querySelector('.mobile-menu__btn');
+        this.DOM.container = document.querySelector('#global-container');
+        this.eventType = this._getEventType();
+        this._addEvent();
+    }
+
+    _getEventType() {
+        return window.ontouchstart ? 'touchstart' : 'click';
+    }
+
+    _toggle(){
+        this.DOM.container.classList.toggle('menu-open');
+    }
+    _addEvent() {
+        this.DOM.btn.addEventListener(this.eventType,this._toggle.bind(this));
+    }
+}
+
+new MobileMenu();
